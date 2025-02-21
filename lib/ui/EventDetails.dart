428e04bfc53bb9569442222ad674e12a9c21d4e9
THExKIDD <tarun22kumar2@gmail.com>
2025-02-21 23:36:58 +0530

    added dynamic ui

--- lib/ui/EventDetails.dart
+++ lib/ui/EventDetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pcte_event_management/LocalStorage/Secure_Store.dart';
 import 'package:pcte_event_management/ui/student_reg.dart';
 
 class EventDetailsPage extends StatelessWidget {
@@ -16,22 +17,36 @@ class EventDetailsPage extends StatelessWidget {
   const EventDetailsPage({super.key,required this.eventName, required this.description, required this.maxStudents, required this.minStudents, required this.location, required this.convener, required this.eventId});
   @override
   Widget build(BuildContext context) {
+    SecureStorage secureStorage = SecureStorage();
     return Scaffold(
       appBar: AppBar(
         title: Text("Event Details", style: TextStyle(color: Colors.white),),
         backgroundColor: Color(0xFF9E2A2F),
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      floatingActionButton: SizedBox(
-        height: 70,
-        width: 70,
-        child: FloatingActionButton(
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (_) => StudentRegistrationScreen(eventId: eventId,)));
-          },
-          backgroundColor: Color(0xFF9E2A2F),
-          child: Icon(Icons.add,color: Colors.white,),
-        ),
+      floatingActionButton: FutureBuilder(
+        future: secureStorage.getData('user_type'),
+        builder: (context, snapshot) {
+
+        if(snapshot.hasData && snapshot.data == 'Teacher')
+          {
+            return SizedBox(
+              height: 70,
+              width: 70,
+              child: FloatingActionButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (_) => StudentRegistrationScreen(eventId: eventId,)));
+                },
+                backgroundColor: Color(0xFF9E2A2F),
+                child: Icon(Icons.add,color: Colors.white,),
+              ),
+            );
+          }
+        else{
+          return SizedBox();
+        }
+
+        },
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
